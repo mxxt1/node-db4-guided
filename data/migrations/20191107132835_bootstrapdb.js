8853c89a8db5650b54cdb456ead9dd59e1505921
mxxt1 <matt@dustcloudlabs.com>
2019-11-07 13:46:43 -0500

    migration table setup complete

---
+++ data/migrations/20191107132835_bootstrapdb.js
@@ -0,0 +1,62 @@
+
+exports.up = function(knex) {
+  return knex.schema
+  .createtable('species', tbl => {
+      tbl.increments();
+      tbl.string('name', 255).notNullable();
+  })
+
+  .createTable('animals', tbl => {
+      tbl.increments();
+
+      tbl.string('name', 255).notNullable();
+
+      tbl.integer('species_id')
+      .unsigned()
+      .references('id')
+      .inTable('species')
+      .onDelete('RESTRICT') //what should happen when the primary key is deleted | other values: 'CASCADE','RESTRICT', 'NO ACTION', 'SET NULL'
+      .onUpdate('CASCADE'); // about changing the value of the primary key | CASCADE changes 
+    
+    })
+
+    .createTable('zoos', tbl => {
+        tbl.increments();
+  
+        tbl.string('name', 255).notNullable();
+
+        tbl.string('address', 255).notNullable();
+    })
+
+    .createTable('animal_zoos', tbl => {
+        tbl.increments();
+  
+        tbl.string('name', 255).notNullable();
+
+        tbl.string('address', 255).notNullable();
+
+        tbl.integer('zoo_id')
+        .unsigned()
+        .references('id')
+        .inTable('zoos')
+        .onDelete('RESTRICT') 
+        .onUpdate('CASCADE');
+
+        tbl.integer('animal_id')
+        .unsigned()
+        .references('id')
+        .inTable('animal')
+        .onDelete('RESTRICT') //what should happen when the primary key is deleted | other values: 'CASCADE','RESTRICT', 'NO ACTION', 'SET NULL'
+        .onUpdate('CASCADE');
+
+        tbl.date('to').notNullable();
+
+        tbl.date('from').notNullable();
+    })
+    
+        
+};
+
+exports.down = function(knex) {
+  
+};
